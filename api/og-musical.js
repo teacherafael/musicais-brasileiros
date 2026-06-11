d463f22e410e3249a121a11765f69a80319ffc50
Rafael <teacherafael@gmail.com>
2026-06-11 00:14:51 -0300

    volta para .js com export default

--- api/og-musical.js
+++ api/og-musical.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   const id = req.query.id
   if (!id) return res.status(400).send("Missing id")
 
