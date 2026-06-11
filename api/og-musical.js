a6abe519909dbd21ddb2cacc560460a03fe5b32b
Rafael <teacherafael@gmail.com>
2026-06-11 00:16:31 -0300

    adiciona package.json na pasta api para forçar commonjs

--- api/og-musical.js
+++ api/og-musical.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const id = req.query.id
   if (!id) return res.status(400).send("Missing id")
 
