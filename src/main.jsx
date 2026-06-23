b58718c4e85b59f338bcbee017ba47233849840a
Rafael <teacherafael@gmail.com>
2026-06-23 16:03:39 -0300

    adiciona Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
+import { inject } from '@vercel/analytics'
 import './index.css'
 import App from './App.jsx'
+
+inject()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -10,6 +13,4 @@ createRoot(document.getElementById('root')).render(
       <App />
     </HelmetProvider>
   </StrictMode>
-
-  
 )
