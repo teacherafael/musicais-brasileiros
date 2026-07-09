8f22d74cdabedc727a9af9e4f01975e24a392a26
Rafael <teacherafael@gmail.com>
2026-07-09 20:03:22 -0300

    corrige endpoint do R2 para jurisdicao EU

--- api/upload-imagem.js
+++ api/upload-imagem.js
@@ -15,7 +15,7 @@ module.exports.config = {
 
 const s3 = new S3Client({
   region: "auto",
-  endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  endpoint: `https://${process.env.R2_ACCOUNT_ID}.eu.r2.cloudflarestorage.com`,
   credentials: {
     accessKeyId: process.env.R2_ACCESS_KEY_ID,
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
