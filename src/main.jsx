0dc8230cfc3fb6c3fe5778b8613d6fda2d2116e7
Rafael <teacherafael@gmail.com>
2026-07-20 12:59:23 -0300

    feat: migra web analytics do Vercel para Cloudflare

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
-import { inject } from '@vercel/analytics'
 import './index.css'
 import App from './App.jsx'
-
-inject()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
